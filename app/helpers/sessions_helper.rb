bd0c2596f84d3d7d3982d15e8c25c209c2579fcb
gir1ng <a08030582573a@gmail.com>
2020-03-25 00:26:07 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
-  # ログインしているユーザーを取得
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      # DBへ問い合わせする回数を減らすための記述
-      @current_use ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
@@ -19,7 +24,39 @@ module SessionsHelper
   end
   
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    # remember_digest属性をDBに保存する
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 永続的セッションを破棄する
+  def forget(user)
+    # remember_digest属性をnilにする
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
   end
 end
